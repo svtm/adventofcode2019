9756df3c6fb4273ec6ddad0a011c9f80d7223396
Eirik Nielsen <eirik.nielsen@signicat.com>
2019-12-10 13:22:47 +0100

    solve day9

--- day9.py
+++ day9.py
@@ -1,9 +1,14 @@
 from util import *
 
-program_string = "104,1125899906842624,99"
+intcode = readline("inputs/9.txt", type=int)[0]
 
-program = list(map(int, program_string.split(",")))
+program_string = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
+#program_string = "1102,34915192,34915192,7,4,7,99,0"
+#intcode = map(int, program_string.split(","))
 
 outp_func = lambda x: print(x)
+inp_func = lambda: 2
 
-run_intcode_program(program, outp_func=outp_func, log_instructions=True)
+program = list(intcode)
+
+run_intcode_program(program, inp_func=inp_func, outp_func=outp_func, log_instructions=False)
